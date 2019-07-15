4cc0be1656139bdf11f4bfc96c8aa4d99fc6b060
Larqqa <32098438+Larqqa@users.noreply.github.com>
2019-07-15 09:23:28 +0300

    Updates

---
+++ precache-manifest.712128b73066b24b7a10fe1d8f926ebf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "461da80017b25e6994b4d91efde56782",
+    "url": "/react-login/index.html"
+  },
+  {
+    "revision": "ffab38f739e3a061513e",
+    "url": "/react-login/static/css/main.bb3c77e9.chunk.css"
+  },
+  {
+    "revision": "5c1f305aa1b04ae1413b",
+    "url": "/react-login/static/js/2.9f885c8e.chunk.js"
+  },
+  {
+    "revision": "ffab38f739e3a061513e",
+    "url": "/react-login/static/js/main.6b210e66.chunk.js"
+  },
+  {
+    "revision": "383624d93bcc0b9c43a6",
+    "url": "/react-login/static/js/runtime~main.50907044.js"
+  }
+]);
